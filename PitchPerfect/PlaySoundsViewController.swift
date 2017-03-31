5262a412831984a774bacbc78ea9d3ae848047f9
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-31 15:26:26 +0800

    adds assets

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -9,8 +9,24 @@
 import UIKit
 
 class PlaySoundsViewController: UIViewController {
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
     var recordedAudioURL: URL!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play sound button pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop button played")
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
